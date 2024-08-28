bff05a9d6811a925b4fa0298bf2f1eaf602d04d2
Michael Leslie <michaelleslie@gmail.com>
2024-08-28 15:09:20 +1000

    Updated main logic to correctly add cogs at load

--- main.py
+++ main.py
@@ -7,24 +7,35 @@ import logging
 import requests
 import os
 from dotenv import load_dotenv
+import utils
 
 load_dotenv()
-token = os.getenv('3CB_TOKEN')
+#print("Environment variables loaded:", os.environ)
+token = os.getenv('BOT_KEY')
+print(token)
 
 intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+bot.state = 'startup'
+cogs = ['cogs.entries','cogs.controls']
 
-def main():
-    bot.run(token)
+async def main():
+    for cog in cogs:
+        await bot.load_extension(cog)
+    utils.initialise_db()
+    await bot.start(token)
+    
 
 @bot.event
 async def on_ready():
     print("Bot online")
     user = await bot.fetch_user(248740105248964608)
     await user.send("Bot online.")
+    await bot.tree.sync()
+    
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
